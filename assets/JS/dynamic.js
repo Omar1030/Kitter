fb965e8a3bd63e7fe889ddf3472bbf7f01d46b61
Omar1030 <omarabdelmoaty816@gmail.com>
2023-11-25 01:04:41 +0200

    Add category section and offer section

---
+++ assets/JS/dynamic.js
@@ -0,0 +1,28 @@
+/* ========== Start Category ========== */
+var swiper = new Swiper(".swiper-category", {
+    cssMode: true,
+    spaceBetween: 25,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        992: {
+            slidesPerView: 5,
+            spaceBetween: 25
+        }
+    }
+});
+/* ========== End Category ========== */
+
+/* ========== Start Offer ========== */
+
+/* ========== End Offer ========== */
